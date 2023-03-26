1238e0133a9bc16cc6a2713b7f0d0f57ed3467b5
Rodrigo Luis Chaves Marques da Silva <rodrigo419@gmail.com>
2023-03-25 22:36:14 -0300

    first commit

---
+++ src/test/java/com/jilosofo/sky/SkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilosofo.sky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
